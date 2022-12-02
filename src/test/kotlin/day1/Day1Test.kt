92bf5fb7fd4c2ee52df8bb20373e8cb3ab365b99
RileyMichael <riley.mcgee.13@gmail.com>
2022-12-01 22:02:35 -0600

    convert tests to kotest string spec

--- src/test/kotlin/day1/Day1Test.kt
+++ src/test/kotlin/day1/Day1Test.kt
@@ -1,35 +1,31 @@
 package com.github.rileymichael.day1
 
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Test
 
-class Day1Test {
-    @Test
-    fun `part 1 example`() {
+class Day1Test : StringSpec({
+    val input = """
+        1000
+        2000
+        3000
+    
+        4000
+    
+        5000
+        6000
+    
+        7000
+        8000
+        9000
+    
+        10000
+    """
+
+    "part 1 example" {
         Day1.solve(Day1::part1, input) shouldBe 24000
     }
 
-    @Test
-    fun `part 2 example`() {
+    "part 2 example" {
         Day1.solve(Day1::part2, input) shouldBe 45000
     }
-
-    companion object {
-        private const val input = """
-            1000
-            2000
-            3000
-
-            4000
-
-            5000
-            6000
-
-            7000
-            8000
-            9000
-
-            10000
-        """
-    }
-}
+})
